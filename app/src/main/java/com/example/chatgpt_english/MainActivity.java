22a3e173ae96aaa2aa2619134db6a48a9d638fa8
Gyeyoung, Jung <gye0203@gmail.com>
2023-11-10 11:42:29 +0900

    added GPT-4 api connection

--- app/src/main/java/com/example/chatgpt_english/MainActivity.java
+++ app/src/main/java/com/example/chatgpt_english/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.chatgpt_english;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
     private TextView responseView;
     private Button sendButton;
+    private OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,52 +48,50 @@ public class MainActivity extends AppCompatActivity {
 
         sendButton.setOnClickListener((view) -> {
             String inputText = userInput.getText().toString();
-//            Log.d("TAG", (new ChatGPTTask().execute(inputText)) + "");
-            responseView.setText(new ChatGPTTask().execute(inputText) + "");
+            postRequest(inputText);
         });
+        client = new OkHttpClient();
 
     }
 
+    private void postRequest(String inputText) {
+        MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
+        String apiKey = "Put API key here";
+        String model = "gpt-4-1106-preview";
+        String postBody = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + inputText + "\"}]}";
 
-
-    private class ChatGPTTask extends AsyncTask<String, Void, String> {
-        String accessToken = "Put access token here";
-
-        protected String doInBackground(String... inputs){
-            String inputText = inputs[0];
-            try{
-                URL url = new URL(
-                        "https://api.openai.com/v1/chat/completions");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Authorization", "Bearer " + accessToken);
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setDoOutput(true);
-
-                String jsonInputString = "{\"prompt\": \"" + inputText + "\", \"max_tokens\": 150}";
-
-                try(OutputStream os = connection.getOutputStream()){
-                    byte[] input = jsonInputString.getBytes("utf-8");
-                    os.write(input, 0 , input.length);
-                }
-
-                try(BufferedReader br = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream(), "utf-8")
-                )){
-                    StringBuilder response = new StringBuilder();
-                    String responseLine = null;
-                    while ((responseLine = br.readLine()) != null){
-                        response.append(responseLine);
-                    }
-
-                    Log.d("TAG", response.toString());
-                    return response.toString();
-                }
-            }catch (Exception e){
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .post(RequestBody.create(postBody, MEDIA_TYPE_JSON))
+                .addHeader("Authorization", "Bearer " + apiKey)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 e.printStackTrace();
-                return null;
+                runOnUiThread(() -> responseView.setText("Failed to connect to the server"));
             }
-        }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    final String responseData = response.body().string();
+
+                    runOnUiThread(() -> {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(responseData);
+                            Log.d("TAG", jsonResponse.toString());
+                            String textResponse = jsonResponse.getJSONArray("choices").getJSONObject(0).getJSONObject("message").getString("content");
+                            responseView.setText(textResponse);
+                        } catch (Exception e){
+                            responseView.setText("Failed to parse the response");
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
