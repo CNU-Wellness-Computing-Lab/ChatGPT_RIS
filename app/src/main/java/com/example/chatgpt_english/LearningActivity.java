1fd5014c048aabfce4dd682942a18809816459d3
Gyeyoung, Jung <gye0203@gmail.com>
2023-11-11 15:59:14 +0900

    added regenerate response

--- app/src/main/java/com/example/chatgpt_english/LearningActivity.java
+++ app/src/main/java/com/example/chatgpt_english/LearningActivity.java
@@ -4,46 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class LearningActivity extends AppCompatActivity {
 
     //test view
     TextView testView;
+    Button testBtn; // 사용자의 주제 변경 또는 더 많은 학습 컨텐츠가 필요한 경우에 사용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
         testView = findViewById(R.id.testView);
-        String gptJsonData = getIntent().getStringExtra("gpt_json_data");
 
-        Log.d("TAG", gptJsonData);
+        String[] parsedContent = getIntent().getStringArrayExtra("parsed_content");
 
-        String gptResponse = null;
+
+        StringBuilder gptResponse = new StringBuilder();
         try {
-            gptResponse = new JSONObject(gptJsonData)
-                    .getJSONArray("choices")
-                    .getJSONObject(0)
-                    .getJSONObject("message")
-                    .getString("content");
-
-            Log.d("TAG", gptResponse);
-
-            String finalGptResponse = gptResponse;
+            assert parsedContent != null;
+            for (String s : parsedContent){
+                gptResponse.append(s).append("\n");
+                Log.d("TAG", s + "\n");
+            }
 
             runOnUiThread(()->{
-                testView.setText(finalGptResponse + "");
+                testView.setText(gptResponse.toString());
             });
-
-        } catch (JSONException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
+        testBtn = findViewById(R.id.testBtn);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToGenerateActivity();
+            }
+        });
+    }
+
+    private void returnToGenerateActivity(){
+        Intent intent = new Intent(this, GenerateActivity.class);
+        startActivity(intent);
+        overridePendingTransition( R.anim.slide_in_left, R.anim.slide_out_right);
     }
 
     @Override
