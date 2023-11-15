3f3b6658d5814d6e9ed5d651f2790bf115ab8945
Gyeyoung, Jung <gye0203@gmail.com>
2023-11-15 15:40:51 +0900

    added voice interaction

--- app/src/main/java/com/example/chatgpt_english/GenerateActivity.java
+++ app/src/main/java/com/example/chatgpt_english/GenerateActivity.java
@@ -9,10 +9,13 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.chatgpt_english.module.TTSModule;
 
 import org.json.JSONObject;
 
@@ -20,6 +23,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.*;
@@ -46,12 +50,17 @@ public class GenerateActivity extends AppCompatActivity {
     private JSONObject jsonResponse;
     private String[] parsedContent;
 
+    private Handler handler;
+    private TTSModule ttsModule;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         responseView = findViewById(R.id.responseView);
+        handler = new Handler();
+        ttsModule = new TTSModule(this);
 
         client = new OkHttpClient.Builder()
                 .readTimeout(60, TimeUnit.SECONDS)
@@ -65,29 +74,31 @@ public class GenerateActivity extends AppCompatActivity {
         englishSkill = sharedPreferences.getString("english_skill", "1");
         topic = sharedPreferences.getString("topic", "랜덤 주제");
 
-        //TODO: 사용자 프로필 중 주제에 따른 영어 학습 컨텐츠 제공
         Log.d("GenerateActivity", "driving skill= " + drivingSkill + "| english skill= " + englishSkill + "| topic= " + topic);
 
         // GPT api response test
         String input = "당신의 역할은 '영어 학습 컨텐츠 제공자'입니다. " +
                 "운전 실력을 1 ~ 10이라고 할 때, 1은 초보자, 10은 숙련자와 같다고 하고, " +
-                "영어 실력을 1 ~ 10이라고 할 때, 1은 초보자, 10은 영어 언어학자 수준하고 같다고 해."+
-                "이때 사용자의 운전 실력은" + drivingSkill + " 영어 실력은" + englishSkill + " 주제는 "+ topic + "으로 설정할 때+" +
-                "사용자의 운전 실력과 영어 실력에 따라 영어 학습 문장 난이도를 조절하고, 주제메 맞는 영어 학습을 하기 위한 영어 문장 5개를 생성해줘."+
-                "단, 영어 단어의 개수가 서로 다른 5개의 문장을 생성해줘  (단어 개수 최소 3개, 최대 5개)" +
+                "영어 실력을 1 ~ 10이라고 할 때, 1은 초보자, 10은 영어 언어학자 수준하고 같다고 해." +
+                "이때 사용자의 운전 실력은" + drivingSkill + " 영어 실력은" + englishSkill + " 주제는 " + topic + "으로 설정할 때+" +
+                "사용자의 운전 실력과 영어 실력에 따라 영어 학습 문장 난이도를 조절하고, 주제메 맞는 영어 학습을 하기 위한 영어 문장 2개를 생성해줘." +
+                "단, 영어 단어의 개수가 서로 다른 2개의 문장을 생성해줘  (단어 개수 최소 3개, 최대 5개)" +
                 "출력 예시는 아래의 예시와 같이 학습을 위한 오직 영어 문장만 출력하고, 이 외 다른 응답은 출력하지마." +
-                "Again, you MUST only say the 5 english sentences for learning and do not contain numbering" +
-                "출력 예시 다음과 같아. I am a boy"
-                ;
+                "Again, you MUST only say the 2 english sentences for learning and do not contain numbering" +
+                "출력 예시 다음과 같아. I am a boy";
         postRequest(input);
 
-        nextBtn.setOnClickListener((v) -> {
-            Intent intent = new Intent(this, LearningActivity.class);
-            intent.putExtra("parsed_content", parsedContent);
+//        nextBtn.setOnClickListener((v) -> {
+//        });
+    }
 
-            startActivity(intent);
-            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
-        });
+    private void goToLearningActivity() {
+        Intent intent = new Intent(this, LearningActivity.class);
+        intent.putExtra("parsed_content", parsedContent);
+
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        ttsModule.shutdown();
     }
 
     @Override
@@ -100,6 +111,7 @@ public class GenerateActivity extends AppCompatActivity {
                 .setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        onDestroy();
                         finishAffinity();
                     }
                 })
@@ -107,12 +119,12 @@ public class GenerateActivity extends AppCompatActivity {
                 .show();
     }
 
-    public String[] parseResponse(String response){
+    public String[] parseResponse(String response) {
         String[] parsedResponse = response.split("\n");
         ArrayList<String> finalResponse = new ArrayList<>();
         //check parsing error
-        for(int i = 0; i < parsedResponse.length; i++){
-            if(!parsedResponse[i].equals("")){
+        for (int i = 0; i < parsedResponse.length; i++) {
+            if (!parsedResponse[i].equals("")) {
                 finalResponse.add(parsedResponse[i]);
             }
         }
@@ -138,14 +150,14 @@ public class GenerateActivity extends AppCompatActivity {
      */
     private void postRequest(String inputText) {
         MediaType MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf-8");
-        String apiKey = "PUT your API KEY here";
+        String apiKey = "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";
         String model = "gpt-4-1106-preview";
         String postBody = "{\"model\": \"" + model + "\", " +
-                        "\"messages\": [" +
-                        "{\"role\": \"user\", " +
-                        "\"content\": \"" + inputText + "\"}" +
-                        "]" +
-                        "}";
+                "\"messages\": [" +
+                "{\"role\": \"user\", " +
+                "\"content\": \"" + inputText + "\"}" +
+                "]" +
+                "}";
 
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/chat/completions")
@@ -179,9 +191,15 @@ public class GenerateActivity extends AppCompatActivity {
                                     getJSONObject("message").
                                     getString("content")
                             );
+
                             responseView.setText("영어 학습 준비 완료!");
-                            nextBtn.setEnabled(true);
-                            nextBtn.setVisibility(View.VISIBLE);
+                            ttsModule.setLanguage(Locale.KOREA);
+                            ttsModule.speak("잠시후 영어 학습을 시작하도록 하겠습니다. 딩동 소리 이후에 들으신 문장을 따라 해 주세요");
+
+                            handler.postDelayed(() -> {goToLearningActivity();}, 10000);
+
+//                            nextBtn.setEnabled(true);
+//                            nextBtn.setVisibility(View.VISIBLE);
 
                         } catch (Exception e) {
                             responseView.setText("Failed to parse the response");
--- app/src/main/java/com/example/chatgpt_english/LearningActivity.java
+++ app/src/main/java/com/example/chatgpt_english/LearningActivity.java
@@ -8,18 +8,21 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.chatgpt_english.module.STTModule;
 import com.example.chatgpt_english.module.TTSModule;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class LearningActivity extends AppCompatActivity {
 
     //test view
-    TextView gptSentenceTextView;
+    TextView ttsSentenceTextView;
     TextView sttResultTextView;
 
     TextView learningResultTextView;
@@ -27,6 +30,8 @@ public class LearningActivity extends AppCompatActivity {
     Button nextSentenceTestBtn;
     Button playSentenceTestBtn;
     private SharedPreferences sharedPreferences;
+    private Handler handler;
+    private Random random;
 
     // tts-stt 모듈
     private TTSModule ttsModule;
@@ -35,26 +40,94 @@ public class LearningActivity extends AppCompatActivity {
     private String[] parsedContent;
     int contentUncompletedIdx = -1;
 
+    // 0: 영어 학습 | 1: 학습 진행 여부 | 2: 새로운 토픽 여부 | 3: 새로운 토픽 묻기
+    int progressStatus = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learning);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-
+        handler = new Handler();
+        random = new Random();
+        progressStatus = 0;
 
         // UI element
-        gptSentenceTextView = findViewById(R.id.testView);
+        ttsSentenceTextView = findViewById(R.id.testView);
         sttResultTextView = findViewById(R.id.testView2);
         learningResultTextView = findViewById(R.id.testView3);
-        regenerateTestBtn = findViewById(R.id.regenTestBtn);
-        nextSentenceTestBtn = findViewById(R.id.nextTestBtn);
-        playSentenceTestBtn = findViewById(R.id.playTestBtn);
 
         sttModule = new STTModule(this, new STTModule.STTListener() {
             @Override
             public void onSTTResult(String result) {
-                runOnUiThread(()->{
+                runOnUiThread(() -> {
                     sttResultTextView.setText(result);
+
+                    if (sttModule.getExpectingYesNoAnswer()) {
+                        // "yes" / "no" 답변을 받아야 하는 프로세스 (학습 진행 여부 / 새로운 토픽 여부 / 새로운 토픽 묻기)
+
+                        /**
+                         *         : 1. 학습 진행 여부 확인 (yes / no) => yes인 경우 다음 프로세스(새로운 토픽 여부) 진행
+                         *         : 2. 새로운 토픽 여부 확인 (yes / no) => yes인 경우 다음 프로세스 (새로운 토픽 묻기) 진행 / no인 경우 기존 토픽으로 재생성
+                         *         : 3. 새로운 토픽 묻기 (자유로운 응답) => 새로운 토픽으로 재생성
+                         */
+
+                        boolean isYes = result.equalsIgnoreCase("yes") || result.equals("예스")
+                                || result.equalsIgnoreCase("yeah") || result.equals("에스")
+                                || result.equals("예") || result.equals("에") || result.equals("응")
+                                || result.equals("네") || result.equals("계속") || result.equals("cancel")
+                                || result.equals("진행") || result.equalsIgnoreCase("chain hang");
+
+                        boolean isNo = result.equalsIgnoreCase("no") || result.equals("노")
+                                || result.equals("아니") || result.equals("아니오") || result.equals("아니요")
+                                || result.equals("중지") || result.equals("충치") || result.equalsIgnoreCase("twenty")
+                                || result.equals("20") || result.equalsIgnoreCase("jonsey")
+                                || result.equalsIgnoreCase("johnsey")
+                                || result.equalsIgnoreCase("choosy")
+                                || result.equalsIgnoreCase("john see")
+                                || result.equalsIgnoreCase("a new")
+                                || result.equalsIgnoreCase("I knew");
+
+                        switch (progressStatus){
+                            case 1: // 학습 진행 여부 확인 (yes / no)
+                                if(isYes){
+                                    Log.d("LearningActivity", "새로운 영어 학습 진행");
+                                    progressStatus = 2;
+                                    scheduleNextSentence("새로운 주제로 영어 학습을 진행하시겠습니까? 진행 또는 아니오로 대답 해 주세요.");
+                                } else if (isNo) {
+                                    Log.d("LearningActivity", "영어 학습 종료");
+                                    goToResultActivity();
+                                } else {
+                                    // 예상 외 답변으로 다시 물어봄
+                                    Log.d("LearningActivity", "예상 외 답변");
+                                    scheduleNextSentence("잘 못 알아들었습니다. 다시 한번 더 말씀 해 주세요");
+                                }
+                                break;
+
+                            case 2: // 새로운 토픽 확인 여부 확인 (yes / no)
+                                if(isYes){
+                                    Log.d("LearningActivity", "새로운 주제로 학습 진행");
+                                    progressStatus = 3;
+                                    scheduleNextSentence("새로운 주제를 말씀 해 주세요.");
+                                } else if (isNo) {
+                                    Log.d("LearningActivity", "기존 주제로 영어 학습으로 종료");
+                                    returnToGenerateActivity();
+                                } else {
+                                    // 예상 외 답변으로 다시 물어봄
+                                    Log.d("LearningActivity", "예상 외 답변");
+                                    scheduleNextSentence("잘 못 알아들었습니다. 다시 한번 더 말씀 해 주세요");
+                                }
+                                break;
+
+                            case 3: // 새로운 주제로 학습 진행
+                                returnToGenerateActivity(result);
+                                break;
+                            default:
+                                break;
+                        }
+                    } else {
+                        scheduleNextSentence(parsedContent);
+                    }
                 });
                 Log.d("LearningActivity", "STT result \n" + result);
             }
@@ -62,16 +135,28 @@ public class LearningActivity extends AppCompatActivity {
             @Override
             public void onSTTError(String errorMessage) {
                 Log.d("LearningActivity", "STT Error \n" + errorMessage);
+                if (!sttModule.getExpectingYesNoAnswer()) {
+                    contentUncompletedIdx--;
+                    // 영어 학습 진행 중, 인식이 제대로 안된 경우 다시 진행 (progressStatus = 0)
+                    runOnUiThread(() -> {
+                        scheduleNextSentence(parsedContent);
+                    });
+                }else{
+                    // progressStatus = 1 / 2 / 3일때 처리 진행
+                    runOnUiThread(() ->{
+                       scheduleNextSentence("잘 못 알아들었습니다. 다시 한번 더 말씀 해 주세요");
+                    });
+                }
             }
 
             @Override
             public void onLearningResult(String result) {
-                runOnUiThread(()->{
+                runOnUiThread(() -> {
                     learningResultTextView.setText(result);
                 });
             }
         });
-        ttsModule = new TTSModule(this, this.sttModule);
+        ttsModule = new TTSModule(this, this.sttModule, false);
 
         parsedContent = getIntent().getStringArrayExtra("parsed_content");
         assert parsedContent != null;
@@ -79,53 +164,62 @@ public class LearningActivity extends AppCompatActivity {
         StringBuilder gptResponse = new StringBuilder();
         contentUncompletedIdx = 0;
 
+        scheduleNextSentence(parsedContent);
+
+        /*
+         * ----- test code starts ------
+         */
+
         // gptResponse 출력 (확인용)
         for (String s : parsedContent) {
             gptResponse.append(s).append("\n");
             Log.d("LearningActivity", s + "\n");
         }
 
-        // 재생성 페이지로 넘어가는 태스트 버튼
-        regenerateTestBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                returnToGenerateActivity("여행");
-            }
-        });
-
-        // text to speech 테스트 버튼
-        playSentenceTestBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runOnUiThread(() -> {
-                    if(contentUncompletedIdx < parsedContent.length){
-                        gptSentenceTextView.setText(sentenceToSpeech(parsedContent, contentUncompletedIdx));
-                    }
-                });
-                nextSentenceTestBtn.setVisibility(View.VISIBLE);
-            }
-        });
-
-        nextSentenceTestBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (contentUncompletedIdx < parsedContent.length-1){
-                    contentUncompletedIdx++;
-                    runOnUiThread(()->{
-                       gptSentenceTextView.setText(parsedContent[contentUncompletedIdx]);
-                       sttResultTextView.setText("waiting for speech-to-text result");
-
-                    });
-                }else{
-                    runOnUiThread(()->{
-                       gptSentenceTextView.setText("영어 학습 끝!");
-                    });
-                }
-            }
-        });
+        /*
+         * ------ Test code ends -------
+         */
 
     }
 
+    private void scheduleNextSentence(String[] _parsedContent) {
+        int delay = random.nextInt(5000) + 3000;
+        if (contentUncompletedIdx < parsedContent.length) {
+            // 학습이 진행되고 있는 상황
+            handler.postDelayed(() -> {
+                runOnUiThread(() -> {
+                    ttsSentenceTextView.setText(sentenceToSpeech(_parsedContent, contentUncompletedIdx++));
+                });
+            }, delay);
+
+        } else {
+            // 학습이 모두 종료된 상황 (영어 학습 단계 종료)
+            progressStatus = 1;
+            sttModule.setExpectingYesNoAnswer(true);
+
+            ttsModule.shutdown();
+            ttsModule = new TTSModule(getApplicationContext(), sttModule, true);
+
+            scheduleNextSentence("영어 학습이 모두 완료되었습니다. 다음 새로운 학습을 진행하시겠습니까?" +
+                            " 계속 또는 아니오로 대답 해 주세요");
+        }
+    }
+
+    private void scheduleNextSentence(String _sentence) {
+        int delay = 3000;
+
+        handler.postDelayed(() -> {
+            runOnUiThread(() -> {
+                ttsSentenceTextView.setText(sentenceToSpeech(_sentence));
+            });
+        }, delay);
+    }
+
+
+
+    /**
+     * tts, stt 객체 파괴를 위한 메소드
+     */
     private void destroyTTSnSTT() {
         if (ttsModule != null) {
             ttsModule.shutdown();
@@ -136,30 +230,69 @@ public class LearningActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * 해당 메소드는 영어 학습 컨텐츠 문장을 사용자에게 제공하기 위해 사용
+     *
+     * @param _parsedContent         영어 학습 컨텐츠
+     * @param _contentUncompletedIdx 학습이 미 완료된 문장
+     * @return 재생된 문장
+     */
     private String sentenceToSpeech(String[] _parsedContent, int _contentUncompletedIdx) {
         String targetSentence = _parsedContent[_contentUncompletedIdx];
 
         assert ttsModule != null;
         Log.d("LearningActivity", targetSentence);
+        ttsModule.setLanguage(Locale.US);
         ttsModule.speak(targetSentence);
         return targetSentence;
+    }
+
+    /**
+     * 해당 메소드는 영어 학습 종료 후, 사용자의 음성 명령을 받기 위한 안내 문장을 재생하기 위해 사용
+     *
+     * @param _sentence 안내 메시지
+     * @return 재생된 문장
+     */
+    private String sentenceToSpeech(String _sentence) {
+        assert ttsModule != null;
+        Log.d("LearningActivity", _sentence);
+        ttsModule.setLanguage(Locale.KOREA);
+        ttsModule.speak(_sentence);
+        return _sentence;
     }
 
     /**
      * 주제가 변경되거나 더 많은 학습 컨텐츠를 제공해야할 시, 호출되는 메서드
      * TODO: 사용자의 음성 명령에 따라 새로운 주제에 따라 재생성하는 동작 구현 필요
      *
-     * @param newTopic 사용자의 새로운 주제
+     * @param _newTopic 사용자의 새로운 주제
      */
-    private void returnToGenerateActivity(String newTopic) {
+    private void returnToGenerateActivity(String _newTopic) {
+        destroyTTSnSTT();
+
         Intent intent = new Intent(this, GenerateActivity.class);
         //변경된 주제에 맞게 변경
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("topic", newTopic);
+        editor.putString("topic", _newTopic);
         editor.apply();
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    private void returnToGenerateActivity(){
         destroyTTSnSTT();
+
+        Intent intent = new Intent(this, GenerateActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    private void goToResultActivity(){
+        destroyTTSnSTT();
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
     @Override
@@ -172,6 +305,7 @@ public class LearningActivity extends AppCompatActivity {
                 .setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
+                        onDestroy();
                         finishAffinity();
                     }
                 })
